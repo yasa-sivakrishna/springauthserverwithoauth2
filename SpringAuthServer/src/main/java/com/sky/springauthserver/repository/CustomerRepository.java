be3604761b1126660c636d0c585a74fe25906009
skylite <yasa.sivakrishna@outlook.com>
2025-05-15 12:38:49 -0500

    Configured spring auth server using mysql database

---
+++ SpringAuthServer/src/main/java/com/sky/springauthserver/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.sky.springauthserver.repository;
+
+
+import com.sky.springauthserver.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    Optional<Customer> findByEmail(String email);
+
+}
